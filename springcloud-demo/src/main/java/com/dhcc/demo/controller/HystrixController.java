da0de5e14b2c0167ae14f8e53db7c32003dcb35a
guogang <guogang@dhcc.com.cn>
2020-12-06 16:14:54 +0800

    修改接口请求方式

--- springcloud-demo/src/main/java/com/dhcc/demo/controller/HystrixController.java
+++ springcloud-demo/src/main/java/com/dhcc/demo/controller/HystrixController.java
@@ -3,6 +3,7 @@ package com.dhcc.demo.controller;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class HystrixController {
     private final Logger logger = LoggerFactory.getLogger(HystrixController.class);
 
     @HystrixCommand(fallbackMethod = "error")
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public Object test() {
 //        String tempMsg = "{sign=f88898b2677e62f1ad54b9e330c0a27e, idcard=130333198901192762, realname=%E5%BE%90%E5%BD%A6%E5%A8%9C, key=c5d34d4c3c71cc45c88f32b4f13da887, mobile=13210141605, bankcard=6226430106137525}";
         String tempMsg = "\"userInfo:{}\", \"{n\" + \"            \"userName\":\"罗志祥\"，n\" + \"            \"idcard\":\"321183197701017846\",n\" + \"            \"password\":\"luozhixiang1234\",n\" + \"            \"mobile\":\"18888888888\"n\" + \"        }\"";
@@ -22,7 +23,7 @@ public class HystrixController {
     }
 
     @HystrixCommand(fallbackMethod = "error")
-    @RequestMapping("/test2")
+    @GetMapping("/test2")
     public Object test2() {
         logger.info("test2");
         return "hello world !";
--- springcloud-demo/src/main/java/com/dhcc/demo/controller/SecureController.java
+++ springcloud-demo/src/main/java/com/dhcc/demo/controller/SecureController.java
@@ -3,6 +3,7 @@ package com.dhcc.demo.controller;
 import com.dhcc.demo.model.CommonModel;
 import com.dhcc.demo.service.secure.SecureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,26 +19,26 @@ public class SecureController {
     @Autowired
     SecureService secureService;
 
-    @RequestMapping("sign")
+    @PostMapping("sign")
     public String sign(@RequestBody CommonModel commonModel) {
         return secureService.sign(commonModel.getData().toString());
     }
 
-    @RequestMapping("verify")
+    @PostMapping("verify")
     public boolean verify(@RequestBody CommonModel commonModel) {
         return secureService.verify(commonModel.getSign(),commonModel.getData().toString());
     }
 
-    @RequestMapping("encrypt")
+    @PostMapping("encrypt")
     public String encrypt(@RequestBody CommonModel commonModel) {
         return secureService.sign(commonModel.getData().toString());
     }
 
-    @RequestMapping("signAndEncrypt")
+    @PostMapping("signAndEncrypt")
     public String signAndEncrypt(@RequestBody  CommonModel commonModel) {
         return secureService.sign(commonModel.getData().toString());
     }
-    @RequestMapping("VerifyAndDecrypt")
+    @PostMapping("VerifyAndDecrypt")
     public String VerifyAndDecrypt(@RequestBody  CommonModel commonModel) {
         return secureService.sign(commonModel.getData().toString());
     }
